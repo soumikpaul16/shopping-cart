491b0f5f79fdaa2d0aec2a45233337a1b52e84f4
soumikpaul16 <soumik.p02@gmail.com>
2021-05-03 21:34:16 +0530

    added navigations in Header

--- src/components/organisms/header/index.js
+++ src/components/organisms/header/index.js
@@ -3,11 +3,19 @@ import { Logo } from '../../molecules';
 import './header.scss';
 
 const Header = () => (
-  <div className="header">
+  <header className="header">
     <div className="header__sub__container">
       <Logo />
+      <nav className="header__nav__items">
+        <span className="header__nav__items__link">Home</span>
+        <span className="header__nav__items__link">Products</span>
+      </nav>
+      <nav className="header__auth__wrapper">
+        <span className="header__auth__wrapper__link sign-in">SignIn</span>
+        <span className="header__auth__wrapper__link">Register</span>
+      </nav>
     </div>
-  </div>
+  </header>
 );
 
 export default Header;
