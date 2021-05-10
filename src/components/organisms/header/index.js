6b98921d02c371b0a23492c48a229020b4e29794
soumikpaul16 <soumik.p02@gmail.com>
2021-05-10 20:23:02 +0530

    PR comment: added metadata

--- src/components/organisms/header/index.js
+++ src/components/organisms/header/index.js
@@ -2,37 +2,20 @@ import React from 'react';
 import { Logo, Nav } from '../../molecules';
 import './header.scss';
 import logoImage from '../../../assets/images/logo.png';
-
-const mainNavs = [
-  { id: 'header.home', path: '', className: 'header__nav__items__link' },
-  {
-    id: 'header.products',
-    path: 'products',
-    className: 'header__nav__items__link',
-  },
-];
-const authNavs = [
-  {
-    id: 'header.sign_in',
-    path: 'signIn',
-    className: 'header__auth__wrapper__link header__auth__wrapper--space-right',
-  },
-  {
-    id: 'header.register',
-    path: 'register',
-    className: 'header__auth__wrapper__link',
-  },
-];
+import metadata from './metadata.json';
 
 const Header = () => (
   <header className="header">
     <div className="header__sub__container">
       <div className="header--left-align">
         <Logo src={logoImage} className="logo" />
-        <Nav className="header__nav__items" navElements={mainNavs} />
+        <Nav className="header__nav__items" navElements={metadata?.headerNavigations?.mainNavs} />
       </div>
       <div>
-        <Nav className="header__auth__wrapper" navElements={authNavs} />
+        <Nav
+          className="header__auth__wrapper"
+          navElements={metadata?.headerNavigations?.authNavs}
+        />
       </div>
     </div>
   </header>
