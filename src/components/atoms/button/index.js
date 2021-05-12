65dfa02e0f6e38dd4139992cbd9471a67a945f13
soumikpaul16 <soumik.p02@gmail.com>
2021-05-13 03:56:30 +0530

    fixed children prop related warnings

--- src/components/atoms/button/index.js
+++ src/components/atoms/button/index.js
@@ -3,23 +3,21 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import './Button.scss';
 
-const Button = ({
-  className, onClick, children, ...props
-}) => (
-  <button onClick={onClick} className={classNames(className, 'button')} {...props}>
-    {children}
-  </button>
+const Button = ({ className, onClick, ...props }) => (
+  <button
+    onClick={onClick}
+    className={classNames(className, 'button')}
+    {...props}
+  />
 );
 
 Button.propTypes = {
   className: PropTypes.string,
   onClick: PropTypes.func,
-  children: PropTypes.string,
 };
 Button.defaultProps = {
   className: '',
   onClick: () => {},
-  children: '',
 };
 
 export default Button;
