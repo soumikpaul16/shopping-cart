c9cba340aee740ae8738f0a6d6aebee8569cb4a6
soumikpaul16 <soumik.p02@gmail.com>
2021-06-10 14:06:13 +0530

    added a padding for home page bottom section

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { GET_BANNERS, GET_CATEGORIES } from '../../../apollo/queries';
 import { Carousel } from '../../molecules';
 import { CategorySection } from '../../organisms';
+import './Home.scss';
 
 const Home = () => {
   const [categories, setCategories] = useState([]);
@@ -33,18 +34,20 @@ const Home = () => {
   return (
     <>
       {banners.length > 0 && <Carousel bannersInfo={banners} />}
-      {categories.length > 0
-        && categories.map((category, index) => (
-          <CategorySection
-            key={category.id}
-            reverseContent={!((index + 1) % 2)}
-            imageUrl={category.imageUrl}
-            name={category.name}
-            description={category.description}
-            buttonContent={category.key}
-            redirectPath={`products/${category.id}`}
-          />
-        ))}
+      <section className="home__categories">
+        {categories.length > 0
+          && categories.map((category, index) => (
+            <CategorySection
+              key={category.id}
+              reverseContent={!((index + 1) % 2)}
+              imageUrl={category.imageUrl}
+              name={category.name}
+              description={category.description}
+              buttonContent={category.key}
+              redirectPath={`products/${category.id}`}
+            />
+          ))}
+      </section>
     </>
   );
 };
